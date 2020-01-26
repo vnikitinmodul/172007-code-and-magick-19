384c522fa7093b6896f1f47adce0eb3d0eeec463
v.nikitin <v.nikitin@modulbank.ru>
2020-01-26 17:48:30 +0500

    Правки перечислений

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var cloud = {
+var Cloud = {
   LEFT: 100,
   TOP: 10,
   WIDTH: 420,
@@ -8,7 +8,7 @@ var cloud = {
   SHADOW_SHIFT: 10
 };
 
-var descr = {
+var Descr = {
   STR_1: 'Ура вы победили!',
   STR_2: 'Список результатов: ',
   LEFT: 130,
@@ -16,21 +16,18 @@ var descr = {
   STR_TOP_2: 75
 };
 
-var col = {
+var Col = {
   WIDTH: 40,
   INDENT: 50,
   LEFT: 150,
   TOP: 220
 };
 
-var color = {
+var Color = {
   RED: 'rgba(255, 0, 0, 1)',
   BLACK: 'rgb(0, 0, 0)',
   WHITE: 'rgb(255, 255, 255)',
-  SHADOW: 'rgba(0, 0, 0, 0.7)',
-  getBlueColor: function () {
-    return 'hsl(237, ' + Math.round(Math.random() * 100) + '%, 50%)';
-  }
+  SHADOW: 'rgba(0, 0, 0, 0.7)'
 };
 
 var ME = 'Вы';
@@ -38,31 +35,35 @@ var FONT = '16px PT Mono';
 var TIME_SHIFT = 10;
 var NAME_SHIFT = 20;
 
+var getBlueColor = function () {
+  return 'hsl(237, ' + Math.round(Math.random() * 100) + '%, 50%)';
+};
+
 var renderCloud = function (ctx) {
-  ctx.fillStyle = color.SHADOW;
-  ctx.fillRect(cloud.LEFT + cloud.SHADOW_SHIFT, cloud.TOP + cloud.SHADOW_SHIFT, cloud.WIDTH, cloud.HEIGHT);
-  ctx.fillStyle = color.WHITE;
-  ctx.fillRect(cloud.LEFT, cloud.TOP, cloud.WIDTH, cloud.HEIGHT);
+  ctx.fillStyle = Color.SHADOW;
+  ctx.fillRect(Cloud.LEFT + Cloud.SHADOW_SHIFT, Cloud.TOP + Cloud.SHADOW_SHIFT, Cloud.WIDTH, Cloud.HEIGHT);
+  ctx.fillStyle = Color.WHITE;
+  ctx.fillRect(Cloud.LEFT, Cloud.TOP, Cloud.WIDTH, Cloud.HEIGHT);
 };
 
 var renderDescr = function (ctx) {
-  ctx.fillStyle = color.BLACK;
+  ctx.fillStyle = Color.BLACK;
   ctx.font = FONT;
-  ctx.fillText(descr.STR_1, descr.LEFT, descr.STR_TOP_1);
-  ctx.fillText(descr.STR_2, descr.LEFT, descr.STR_TOP_2);
+  ctx.fillText(Descr.STR_1, Descr.LEFT, Descr.STR_TOP_1);
+  ctx.fillText(Descr.STR_2, Descr.LEFT, Descr.STR_TOP_2);
 };
 
 var renderCol = function (ctx, colParam) {
-  ctx.fillRect(colParam.leftPos, colParam.topPos - colParam.colHeight, col.WIDTH, colParam.colHeight);
+  ctx.fillRect(colParam.leftPos, colParam.topPos - colParam.colHeight, Col.WIDTH, colParam.colHeight);
 };
 
 var renderTime = function (ctx, colParam) {
-  ctx.fillStyle = color.BLACK;
+  ctx.fillStyle = Color.BLACK;
   ctx.fillText(Math.round(colParam.time), colParam.leftPos, (colParam.topPos - TIME_SHIFT - colParam.colHeight));
 };
 
 var renderName = function (ctx, colParam) {
-  ctx.fillStyle = color.BLACK;
+  ctx.fillStyle = Color.BLACK;
   ctx.fillText(colParam.name, colParam.leftPos, colParam.topPos + NAME_SHIFT);
 };
 
@@ -86,17 +87,17 @@ window.renderStatistics = function (ctx, names, times) {
 
   for (var j = 0; j < names.length; j++) {
     var colParam = {
-      leftPos: col.LEFT + (col.WIDTH + col.INDENT) * j,
-      topPos: col.TOP,
+      leftPos: Col.LEFT + (Col.WIDTH + Col.INDENT) * j,
+      topPos: Col.TOP,
       colHeight: times[j] / maxValue * 100,
       name: names[j],
       time: times[j]
     };
 
     if (names[j] === ME) {
-      ctx.fillStyle = color.RED;
+      ctx.fillStyle = Color.RED;
     } else {
-      ctx.fillStyle = color.getBlueColor();
+      ctx.fillStyle = getBlueColor();
     }
 
     renderCol(ctx, colParam);

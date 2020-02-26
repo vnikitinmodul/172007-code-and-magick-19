0d9282b86ac8af5c685edb1d88c95a8b6b625eb8
v.nikitin <v.nikitin@modulbank.ru>
2020-02-26 16:02:28 +0500

    Учебный проект: pimp my mage Вынесен повторяющийся код

--- js/backend.js
+++ js/backend.js
@@ -1,17 +1,18 @@
 'use strict';
 
 (function () {
-  var URL_WIZARDS = 'https://js.dump.academy/code-and-magick/data';
-  var URL_UPLOAD = 'https://js.dump.academy/code-and-magik';
+  var URL = 'https://js.dump.academy/code-and-magick';
+  var LOAD_METHOD = '/data';
   var XHR_TIMEOUT = 10000;
+  var STATUS_SUCCESS = 200;
 
   window.backend = {
-    load: function (onLoad, onError) {
+    initXhr: function (onLoad, onError, url, type) {
       var xhr = new XMLHttpRequest();
       xhr.responseType = 'json';
 
       xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
+        if (xhr.status === STATUS_SUCCESS) {
           onLoad(xhr.response);
         } else {
           onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
@@ -28,32 +29,16 @@
 
       xhr.timeout = XHR_TIMEOUT;
 
-      xhr.open('GET', URL_WIZARDS);
+      xhr.open(type, url);
+
+      return xhr;
+    },
+    load: function (onLoad, onError) {
+      var xhr = this.initXhr(onLoad, onError, URL + LOAD_METHOD, 'GET');
       xhr.send();
     },
     save: function (data, onLoad, onError) {
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
-          onLoad(xhr.response);
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения');
-      });
-
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-      });
-
-      xhr.timeout = XHR_TIMEOUT;
-
-      xhr.open('POST', URL_UPLOAD);
+      var xhr = this.initXhr(onLoad, onError, URL, 'POST');
       xhr.send(data);
     }
   };
